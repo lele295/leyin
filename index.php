d2278581c55e43024b4ee6b2ca2c5729f69d67c3
yinle <le.yin@seeed.cc>
2021-04-04 23:20:52 +0800

    第一次提交

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zou_mj
+ * Date: 2021/4/4
+ * Time: 22:42
+ */
+
+require 'vendor/autoload.php';
+$pack = new \leyin\package();
+$pack->index();
---
+++ src/leyin/package.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zou_mj
+ * Date: 2021/4/4
+ * Time: 22:40
+ */
+
+namespace leyin;
+
+
+class package
+{
+
+    public function index()
+    {
+        phpinfo();
+    }
+
+}
